a97fde0910b55d7ca6fdb8e300ea87043b5db46d
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-24 17:40:59 +0700

    update: seasons page

--- src/app/seasons/page.tsx
+++ src/app/seasons/page.tsx
@@ -3,8 +3,14 @@ import { Button } from "@/components/ui/button";
 import { BiSolidLeftArrow, BiSolidRightArrow } from "react-icons/bi";
 import React, { useEffect, useState } from "react";
 import { SeasonsClient, Anime, JikanPagination } from "@tutkli/jikan-ts";
-import Card from "@/components/card";
 import Loadingg from "@/components/loadingg";
+import CardNew from "@/components/cardNew";
+import { AiOutlineArrowUp } from "react-icons/ai";
+import clsx from "clsx";
+import { getSeason } from "@/utils/getSeason";
+import { Select } from "@/components/ui/select";
+import { yearOptions } from "@/constants/years";
+import { seasonOptions } from "@/constants/seasons";
 
 type SeasonProps = {
   data: Anime[];
@@ -14,13 +20,20 @@ type SeasonProps = {
 const Season = () => {
   const seasonsClient = new SeasonsClient();
   const [seasonData, setSeasonData] = useState<SeasonProps | null>(null);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showButton, setShowButton] = useState<boolean>(false);
+  const nowYear = new Date().getFullYear();
+  const [year, setYear] = useState<number>(nowYear);
+  const [season, setSeason] = useState<string>(getSeason);
 
-  const fetchData = async (page: number) => {
+  const fetchData = async (page: number, year: number, season: string) => {
     setIsLoading(true);
     try {
-      const res: SeasonProps = await seasonsClient.getSeasonNow({ page });
+      //@ts-ignore
+      const res: SeasonProps = await seasonsClient.getSeason(year, season, {
+        page,
+      });
       setSeasonData(res);
     } catch (error) {
       setIsLoading(false);
@@ -30,8 +43,9 @@ const Season = () => {
   };
 
   useEffect(() => {
-    fetchData(currentPage);
-  }, [currentPage]);
+    fetchData(currentPage, year, season);
+    scrollToTop();
+  }, [currentPage, year, season]);
 
   const handleNextPage = () => {
     setCurrentPage(currentPage + 1);
@@ -43,14 +57,45 @@ const Season = () => {
     }
   };
 
+  const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
+  const handleScroll = () =>
+    window.scrollY > 500 ? setShowButton(true) : setShowButton(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <section className="flex flex-col my-2">
-      <div className="bg-white py-1 px-4 rounded-lg mx-2 sticky top-[80px] z-20 flex justify-between mb-2 items-center">
-        <div className="flex gap-2">
+    <section className="flex flex-col my-2 ">
+      <Button
+        className={clsx(
+          `fixed bottom-4 right-4 bg-blue-500 text-white p-3 rounded-full`,
+          showButton ? "block" : "hidden"
+        )}
+        onClick={scrollToTop}
+      >
+        <AiOutlineArrowUp />
+      </Button>
+      <div className="bg-white py-1 px-4 rounded-lg mx-2 my-2 sticky top-[80px] z-20 flex justify-between items-center">
+        <div className="flex gap-2 items-center">
           <h2 className="font-bold">SEASON</h2>
+          <Select
+            value={year}
+            options={yearOptions}
+            onChange={(e) => setYear(parseInt(e.target.value))}
+          />
+          <Select
+            value={season}
+            options={seasonOptions}
+            onChange={(e) => setSeason(e.target.value)}
+          />
         </div>
         <div className="flex gap-2 items-center">
-          <p className="text-xs">PAGE {currentPage}</p>
+          <p className="hidden md:block text-xs">PAGE {currentPage}</p>
           <Button
             disabled={currentPage === 1}
             size={"sm"}
@@ -70,9 +115,9 @@ const Season = () => {
       {isLoading ? (
         <Loadingg />
       ) : (
-        <div className="flex gap-4 flex-wrap justify-center">
+        <div className="flex gap-4 flex-wrap justify-center px-2">
           {seasonData?.data.map((val, i) => (
-            <Card key={i} val={val} />
+            <CardNew key={i} val={val} />
           ))}
         </div>
       )}
