bab3b5fde9b89a56646156d6b64f846a2a9a92bd
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-25 16:28:45 +0700

    update: components

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,39 +9,41 @@ const Card = ({ val }: { val: Anime }) => {
   const [hover, setHover] = useState(false);
   const router = useRouter();
   return (
-    <div
-      className="relative cursor-pointer"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-      onClick={() => router.push("/")}
-    >
+    <Link href={`/anime/${val.mal_id}`}>
       <div
-        className={clsx(
-          `h-full z-10 w-full absolute flex justify-center items-center bg-black transition-opacity`,
-          hover ? "bg-opacity-50" : "bg-opacity-0",
-          hover ? "scale-105" : "scale-95"
-        )}
+        className="relative cursor-pointer"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        onClick={() => router.push("/")}
       >
-        <p
+        <div
           className={clsx(
-            `font-bold absolute bottom-0 left-0 p-2 text-white`,
-            hover ? "opacity-100" : "opacity-0"
+            `h-full z-10 w-full absolute flex justify-center items-center bg-black transition-opacity`,
+            hover ? "bg-opacity-50" : "bg-opacity-0",
+            hover ? "scale-105" : "scale-95"
           )}
         >
-          {val.title}
-        </p>
+          <p
+            className={clsx(
+              `font-bold absolute bottom-0 left-0 p-2 text-white`,
+              hover ? "opacity-100" : "opacity-0"
+            )}
+          >
+            {val.title}
+          </p>
+        </div>
+        <Image
+          src={val.images.jpg.large_image_url!}
+          alt="cover"
+          width={200}
+          height={300}
+          className={clsx(
+            `object-cover max-h-52 w-40 md:max-h-72 md:w-52 h-full`,
+            hover ? "scale-105" : "scale-95"
+          )}
+        />
       </div>
-      <Image
-        src={val.images.jpg.large_image_url!}
-        alt="cover"
-        width={200}
-        height={300}
-        className={clsx(
-          `object-cover max-h-52 w-40 md:max-h-72 md:w-52 h-full`,
-          hover ? "scale-105" : "scale-95"
-        )}
-      />
-    </div>
+    </Link>
   );
 };
 
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,21 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full bg-black text-white flex mt-2 justify-center p-3 flex-col border-t-2 border-white ">
+      <div className="flex flex-wrap justify-center gap-2">
+        <p> Designed and developed with ❤️ by</p>
+        <a
+          className="hover:underline text-rose-600"
+          href="https://fahminurkamil.vercel.app/"
+          target="_blank"
+        >
+          @fahminurk
+        </a>
+      </div>
+      <div className="flex justify-center">
+        <p>Powered by Jikan.moe</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
