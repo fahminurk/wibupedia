bfda57e9e58ba9cedefbf9a1852d59f79900fd56
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-25 16:28:11 +0700

    update: constants

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const seasonOptions = ["spring", "summer", "fall", "winter"];
+export const yearOptions = ["2023", "2022", "2021", "2020", "2019", "2018"];
+export const countyOptions = ["Japanese", "English"];
--- src/constants/seasons.ts
+++
@@ -1,1 +0,0 @@
-export const seasonOptions = ["spring", "summer", "fall", "winter"];
