a93b180f91c5da9f648a045090a8f2505003fbc8
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-26 14:31:19 +0700

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,12 @@
 import Link from "next/link";
 import { InputGroup } from "./ui/inputGroup";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 const Navbar = () => {
+  const [keyword, setKeyword] = useState<string>("");
+  const router = useRouter();
+
   return (
     <nav className="bg-black z-50 flex justify-between p-4 sticky top-0 border-b-2 border-white items-center">
       <Link href={"/"}>
@@ -11,7 +16,12 @@ const Navbar = () => {
 
       <InputGroup
         placeholder="Search anime"
-        onClick={() => {}}
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        onClick={() => {
+          router.push(`/search/${keyword.replace(" ", "-")}`);
+          setKeyword("");
+        }}
         icon={<FaSearch />}
       />
     </nav>
