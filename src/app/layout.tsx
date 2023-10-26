18155dc2a26cf1d440c4919420856f676f79086c
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-10-26 14:29:44 +0700

    update: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,30 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Wibupedia",
   description: "anime list up to date",
+  keywords: [
+    "anime",
+    "wibupedia",
+    "myanimelist",
+    "anime list",
+    "naruto",
+    "one piece",
+    "jujutsu kaizen",
+  ],
+  authors: [{ name: "fahmi nurkamil", url: "https://github.com/fahminurk" }],
+  viewport: "width=device-width, initial-scale=1",
+  openGraph: {
+    type: "website",
+    locale: "id",
+    url: "https://wibupedia.vercel.app",
+    title: "Wibupedia",
+    description: "anime list up to date",
+    images: [
+      {
+        url: "https://i.imgur.com/CYsg2Gs.png",
+      },
+    ],
+    siteName: "Wibupedia",
+  },
 };
 
 export default function RootLayout({
